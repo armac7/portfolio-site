51bdd3570b03a6464317b1381d5526cea895dbd9
armac7 <w.mcd0320@icloud.com>
2026-01-20 11:41:29 -0700

    Removed Jquery, used native JS

--- scripts/load-components.js
+++ scripts/load-components.js
@@ -1,3 +1,15 @@
-$(function () {
-    $('#navbar').load('/components/navbar.html');  
+document.addEventListener("DOMContentLoaded", async function() {
+    // Load navbar
+    const navbar = document.getElementById("navbar");
+    if (!navbar) return;
+
+    try {
+        const response = await fetch("./components/navbar.html");
+        if (!response.ok) throw new Error("Failed to load navbar");
+
+        navbar.innerHTML = await response.text();
+    }
+    catch (err) {
+        console.error(err);
+    }
 });
